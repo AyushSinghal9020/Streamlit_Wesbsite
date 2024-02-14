bde3f636bfb98136d2c4191e236365940b75195d
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2024-02-14 14:11:08 +0530

    Update app.py

--- Main/app.py
+++ Main/app.py
@@ -1,11 +1,7 @@
 import streamlit as st
 from copy import deepcopy
 from PIL import Image
-import torch
-import os 
-
 import streamlit.components.v1 as components
-from gradio_client import Client
 
 from helper import (
     check_api , 
@@ -21,118 +17,57 @@ import torch
 from torchvision.utils import save_image
 from presidio_analyzer import AnalyzerEngine
 
-# st.set_page_config(layout = 'wide')
+st.set_page_config(layout = 'wide')
 
 themes = {
     '0' : [
         'Main/Assets/Background/Road.jpg' , 
         '# Road - Where the journey never ends and so does the traffic.' , 
-        'If you dont like the Road theme, you can change it by clicking on the `Generate Images` Button' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Road/AI Gen-Text Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Road/PII Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Road/HUBMAP.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Road/LLM Exam.png' , 
-        'Main/Assets/Project Tiles/Research/Road/VAl_rind.png' , 
-        'Main/Assets/Project Tiles/Development/Road/VA_dev.png'
+        'If you dont like the Road theme, you can change it by clicking on the `Generate Images` Button'
         ] ,
     '1' : [
         'Main/Assets/Background/Nature.jpg' , 
         '# No, you cannot eat that Mushroom' , 
-        'If you dont like the Nature theme, you can change it by clicking on the `Generate Images` Button' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Nature/AI Gen-Text Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Nature/PII Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Nature/HUBMAP.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Nature/LLM Exam.png' ,
-        'Main/Assets/Project Tiles/Research/Nature/VAl_rind.png' ,
-        'Main/Assets/Project Tiles/Development/Nature/VA_dev.png'
+        'If you dont like the Nature theme, you can change it by clicking on the `Generate Images` Button'
         ] , 
     '2' : [
         'Main/Assets/Background/City.jpg' , 
-        '# City - Where the lights never go out and so does the noise.' , 
-        'If you dont like the City theme, you can change it by clicking on the `Generate Images` Button' ,  
-        'Main/Assets/Project Tiles/ML_DL_AI/City/AI Gen-Text Detection.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/City/PII Detection.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/City/HUBMAP.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/City/LLM Exam.png' ,
-        'Main/Assets/Project Tiles/Research/City/VAl_rind.png' ,
-        'Main/Assets/Project Tiles/Development/City/VA_dev.png'
+        '# City - Where the lights never go out and so does the noise.'
         ] ,
     '3' : [
         'Main/Assets/Background/Space.jpg' , 
         '# No, you cannot pee there'  ,  
-        'If you dont like the Space theme, you can change it by clicking on the `Generate Images` Button' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Space/AI Gen-Text Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Space/PII Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Space/HUBMAP.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Space/LLM Exam.png' ,
-        'Main/Assets/Project Tiles/Research/Space/VAl_rind.png' ,
-        'Main/Assets/Project Tiles/Development/Space/VA_dev.png'
+        'If you dont like the Space theme, you can change it by clicking on the `Generate Images` Button'
         ] ,
     '4' : [
         'Main/Assets/Background/Sea.jpg' , 
         '# Every sea is just a huge Pond' , 
-        'If you dont like the Sea theme, you can change it by clicking on the `Generate Images` Button' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Sea/AI Gen-Text Detection.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Sea/PII Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Sea/HUBMAP.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Sea/LLM Exam.png' ,
-        'Main/Assets/Project Tiles/Research/Sea/VAl_rind.png' ,
-        'Main/Assets/Project Tiles/Development/Sea/VA_dev.png'
+        'If you dont like the Sea theme, you can change it by clicking on the `Generate Images` Button'
         ] ,
     '5' : [
         'Main/Assets/Background/Plane.jpg' , 
         '# No, your phone will not survive that high' ,
-        'If you dont like the Plane theme, you can change it by clicking on the `Generate Images` Button' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Plane/AI Gen-Text Detection.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Plane/PII Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Plane/HUBMAP.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Plane/LLM Exam.png' ,
-        'Main/Assets/Project Tiles/Research/Plane/VAl_rind.png' ,
-        'Main/Assets/Project Tiles/Development/Plane/VA_dev.png'
+        'If you dont like the Plane theme, you can change it by clicking on the `Generate Images` Button'
         ] ,
     '6' : [
         'Main/Assets/Background/Mountain.jpg' , 
         '# Mountain - Where the air is thin and so are the chances of survival' , 
-        'If you dont like the Mountain theme, you can change it by clicking on the `Generate Images` Button' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Mountain/AI Gen-Text Detection.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Mountain/PII Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Mountain/HUBMAP.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Mountain/LLM Exam.png' ,
-        'Main/Assets/Project Tiles/Research/Mountain/VAl_rind.png' ,
-        'Main/Assets/Project Tiles/Development/Mountain/VA_dev.png'
+        'If you dont like the Mountain theme, you can change it by clicking on the `Generate Images` Button'
         ] ,
     '7' : [
         'Main/Assets/Background/WaterFall.jpg' , 
         '# WaterFall - Natures way to felx' , 
-        'If you dont like the WaterFall theme, you can change it by clicking on the `Generate Images` Button' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/WaterFall/AI Gen-Text Detection.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/WaterFall/PII Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/WaterFall/HUBMAP.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/WaterFall/LLM Exam.png' ,
-        'Main/Assets/Project Tiles/Research/WaterFall/VAl_rind.png' ,
-        'Main/Assets/Project Tiles/Development/WaterFall/VA_dev.png'
+        'If you dont like the WaterFall theme, you can change it by clicking on the `Generate Images` Button'
         ] ,
     '8' : [
         'Main/Assets/Background/Fish.png' , 
         '# No, they cannot swim in stomach' , 
-        'If you dont like the Fish theme, you can change it by clicking on the `Generate Images` Button' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Fish/AI Gen-Text Detection.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Fish/PII Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Fish/HUBMAP.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Fish/LLM Exam.png' ,
-        'Main/Assets/Project Tiles/Research/Fish/VAl_rind.png' ,
-        'Main/Assets/Project Tiles/Development/Fish/VA_dev.png'
+        'If you dont like the Fish theme, you can change it by clicking on the `Generate Images` Button'
         ] ,
     '9' : [
         'Main/Assets/Background/Desert.jpg' , 
         '# Desert - Where nature hosts Mirage fashion shows' , 
-        'If you dont like the Desert theme, you can change it by clicking on the `Generate Images` Button' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Desert/AI Gen-Text Detection.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Desert/PII Detection.png' , 
-        'Main/Assets/Project Tiles/ML_DL_AI/Desert/HUBMAP.png' ,
-        'Main/Assets/Project Tiles/ML_DL_AI/Desert/LLM Exam.png' ,
-        'Main/Assets/Project Tiles/Research/Desert/VAl_rind.png' ,
-        'Main/Assets/Project Tiles/Development/Desert/VA_dev.png' 
+        'If you dont like the Desert theme, you can change it by clicking on the `Generate Images` Button' 
         ]
 }
 
@@ -184,79 +119,94 @@ def home() :
 
     set_bg_hack(themes[str(theme_number)][0])
 
-    if col_1.button('Sources') : 
-
-        st.write(theme_number)
-        for _ in range(7) : col_2.markdown('')
-
-        col_2.code(open('Main/Assets/Code/Classifier.py').read())
-        col_1.code(open('Main/Assets/Code/GAN.py').read())
-        with col_2 : components.iframe('https://api.wandb.ai/links/ayushsinghal659/i4ta3isp', height=600 , scrolling = True)
-        with col_1 : components.iframe('https://api.wandb.ai/links/ayushsinghal659/tm39kito', height=600 , scrolling = True)
-
     st.markdown(open('Main/Assets/TextFiles/Projects.txt').read() , unsafe_allow_html = True)
 
     col_1 , col_2  = st.columns(2)
 
+    col_1.write('Use Navigation to go to specific project')
+        
+    st.markdown(open('Main/Assets/TextFiles/Deep Learning Projects.txt').read() , unsafe_allow_html = True)
     
-    with col_1 : 
+    col_1 , col_2 = st.columns(2)
 
-        st.write('Use Navigation to go to specific project')
-        st.markdown(open('Main/Assets/TextFiles/Deep Learning Projects.txt').read() , unsafe_allow_html = True)
-        st.markdown(f'''
-            <img 
-                src="data:image/jpeg;base64,{get_image_base64_str(themes[str(theme_number)][3])}" 
-                width="300" 
-                height="200">
-        ''' , unsafe_allow_html = True)
-        for _ in range(4) : st.markdown('')
-        st.markdown(f'''
-            <img
-                src="data:image/jpeg;base64,{get_image_base64_str(themes[str(theme_number)][4])}"
-                width="300"
-                height="200">
-        ''' , unsafe_allow_html = True)
+    col_1.markdown(f'''
+        <img 
+            src="data:image/jpeg;base64,{get_image_base64_str('Main/Assets/Project Tiles/Echos Of The Canvas.jpeg')}" 
+            width="400" 
+            height="200">
+    ''' , unsafe_allow_html = True)
+    col_2.markdown(f'''
+        <img
+            src="data:image/jpeg;base64,{get_image_base64_str('Main/Assets/Project Tiles/Behind The Text.jpeg')}"
+            width="400"
+            height="200">
+    ''' , unsafe_allow_html = True)
+    col_1.markdown(f'''
+        <img
+            src="data:image/jpeg;base64,{get_image_base64_str('Main/Assets/Project Tiles/Beyond The Visiible.jpeg')}"
+            width="400"
+            height="200">
+    ''' , unsafe_allow_html = True)
 
-        st.markdown(open('Main/Assets/TextFiles/Research Projects.txt').read() , unsafe_allow_html = True)
-        st.markdown(f'''
-            <img
-                src="data:image/jpeg;base64,{get_image_base64_str(themes[str(theme_number)][7])}"
-                width="300" 
-                height="200"> 
-        ''' , unsafe_allow_html = True)
+    st.markdown(open('Main/Assets/TextFiles/Research Projects.txt').read() , unsafe_allow_html = True)
+    
+    
+    st.markdown(f'''
+        <img
+            src="data:image/jpeg;base64,{get_image_base64_str('Main/Assets/Project Tiles/VAlrind.jpeg')}"
+            width="400" 
+            height="200"> 
+    ''' , unsafe_allow_html = True)
 
-        st.markdown(open('Main/Assets/TextFiles/Development Projects.txt').read() , unsafe_allow_html = True)
-        st.markdown(f'''
-            <img 
-                src="data:image/jpeg;base64,{get_image_base64_str(themes[str(theme_number)][8])}" 
-                width="300" 
-                height="200">
-        ''' , unsafe_allow_html = True)
-
-    with col_2 : 
-
-        for _ in range(6) : st.markdown('')
-        st.markdown(f'''
-            <img
-                src="data:image/jpeg;base64,{get_image_base64_str(themes[str(theme_number)][5])}"
-                width="300"
-                height="200">
-        ''' , unsafe_allow_html = True)
-
-        # for _ in range(4) : st.markdown('')
-        # st.markdown(f'''
-        # <a href = 'https://www.google.com'>
-        #     <img
-        #         src="data:image/jpeg;base64,{get_image_base64_str(themes[str(theme_number)][6])}"
-        #         width="400"
-        #         height="200">
-        # </a>
-        # ''' , unsafe_allow_html = True)
-
+    st.markdown(open('Main/Assets/TextFiles/Development Projects.txt').read() , unsafe_allow_html = True)
+    st.markdown(f'''
+        <img 
+            src="data:image/jpeg;base64,{get_image_base64_str('Main/Assets/Project Tiles/VAdev.jpeg')}" 
+            width="400" 
+            height="200">
+    ''' , unsafe_allow_html = True)
 
     st.markdown(open('Main/Assets/TextFiles/Accomplishments.txt').read() , unsafe_allow_html = True)
     st.markdown(open('Main/Assets/TextFiles/Accomplishments Markdown.txt').read() , unsafe_allow_html = True)
-    st.markdown(open('Main/Assets/TextFiles/How To Reach Me.txt').read() , unsafe_allow_html = True)
+
+    col_1 , col_2 , col_3 , col_4 , col_5 , col_6 , col_7 , col_8 , col_9 , col_10 , col_11 , col_12 , col_13 , col_14 , col_15 = st.columns(15)
+
+    col_1.markdown(f'''
+<a href = 'ayushsinghal659@gmail.com'>
+    <img 
+        src="data:image/jpeg;base64,{get_image_base64_str('Main/Assets/Reach/Mail.png')}" 
+        width="60" 
+        height="50">
+</a>
+''' , unsafe_allow_html = True)
+
+    col_2.markdown(f'''
+<a href = 'https://github.com/AyushSinghal9020'>
+    <img 
+        src="data:image/jpeg;base64,{get_image_base64_str('Main/Assets/Reach/Github.png')}" 
+        width="60" 
+        height="50">
+</a>
+''' , unsafe_allow_html = True)
+
+    col_3.markdown(f'''
+<a href = 'https://www.kaggle.com/ayushs9020'>
+    <img 
+        src="data:image/jpeg;base64,{get_image_base64_str('Main/Assets/Reach/Kaggle.png')}" 
+        width="70" 
+        height="50">
+</a>
+''' , unsafe_allow_html = True)
+
+    col_4.markdown(f'''
+<a href = 'www.linkedin.com'>
+    <img 
+        src="data:image/jpeg;base64,{get_image_base64_str('Main/Assets/Reach/Linkdin.webp')}" 
+        width="50" 
+        height="50">
+</a>
+''' , unsafe_allow_html = True)
+
 def image_to_music() : 
     '''
     Image to Music Page
